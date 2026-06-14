9b077e47af1065f5855cfe265e1c9da5ae6a1248
Ashutosh Singh <ashutoshsinghthakur0827@gmail.com>
2026-06-14 10:48:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll("button");
+const cards = document.querySelectorAll(".card");
+
+buttons.forEach(button=>{
+button.addEventListener("click",()=>{
+
+let filter = button.getAttribute("data-filter");
+
+cards.forEach(card=>{
+
+if(filter==="all"){
+card.style.display="block";
+}
+else if(card.classList.contains(filter)){
+card.style.display="block";
+}
+else{
+card.style.display="none";
+}
+
+});
+
+});
+});
